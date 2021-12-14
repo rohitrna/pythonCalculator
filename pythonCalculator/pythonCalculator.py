013530f7965a73fa739de094c9b612824f92ea52
rohitrna <rohitraghunath3@gmail.com>
2021-12-13 20:25:48 -0800

    Add files via upload

---
+++ pythonCalculator/pythonCalculator.py
@@ -0,0 +1,19 @@
+
+
+print("Calculator Project")
+
+def addition( add1 , add2):
+    
+    return(ans)
+    
+def subtraction(sub1 , sub2):
+    return(ans)
+
+def multiplication(mul1 , mul2):
+    
+    return(ans)
+    
+def division(div1 , div2):
+    
+    return(ans)
+
